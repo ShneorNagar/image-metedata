3c714ce7898f9ffebe16bee061538cf184ff0061
ShneorNagar <shneor30@gmail.com>
2021-02-21 18:14:49 +0200

    report tree is now sorted

--- src/app/shared/tree-converter.pipe.ts
+++ src/app/shared/tree-converter.pipe.ts
@@ -6,16 +6,16 @@ import {Pipe, PipeTransform} from "@angular/core";
 export class TreeConverterPipe implements PipeTransform{
 
     transform(value: any): any {
-        console.log('pipe works...')
         return this.buildNestedReportAsTree(value);
     }
 
     buildNestedReportAsTree(reports) {
 
         let repAsArray: any[] = [];
+        const sortedKeys = Object.keys(reports).sort();
 
         function recurse(reports, current?) {
-            for (const key in reports) {
+            for (const key of sortedKeys) {
                 let value = reports[key];
                 if (value != undefined) {
                     if (value && typeof value === 'object') {
@@ -46,7 +46,22 @@ export class TreeConverterPipe implements PipeTransform{
                 }
             }
         }
+
+        function extractMakerNoteInfo(makerNote: number[]){
+            return String.fromCharCode(...makerNote);
+        }
+
         recurse(reports);
+
+        if (reports.MakerNote){
+            repAsArray.push({
+                label: `MakerNote`,
+                children: [{
+                    label: `MarkerNote - ${extractMakerNoteInfo(reports.MakerNote)}`
+                }]
+            })
+        }
+
         return repAsArray;
     }
 }
